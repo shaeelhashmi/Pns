e93ab20f6a6a9f0d16ed87ea00640e4fc5c057d6
shaeelhashmi <shaeelh001@gmail.com>
2025-09-26 23:41:13 +0500

    Added another author

--- src/Components/About/Utils/Advisory.tsx
+++ src/Components/About/Utils/Advisory.tsx
@@ -6,12 +6,13 @@ export default function Advisory() {
     { img: './JavedAkhtar.png', Name: 'Javed Akhtar', Title: 'Chief Editor (Chemistry, Materials Sciences)', Affiliation: 'Pakistan Academy of Sciences, Pakistan' },
     { img: './ShahabKhushnood.png', Name: 'Shahab Khushnood', Title: 'Chief Editor (Engineering)', Affiliation: 'University of Wah, Pakistan' },
     { img: './MuhammadNasirKhan.png', Name: 'Muhammad Nasir Khan', Title: 'Section Editor (Mathematics)', Affiliation: 'International Islamic University, Islamabad, Pakistan' },
+    {img:'./yasir.jpg', Name: 'Yasar Ayaz', Title: 'Chief Editor (Artificial Intelligence)', Affiliation: 'National University of Sciences and Technology (NUST), Pakistan' }
   ]
 
   return (
     <div className="grid xl:grid-cols-2 grid-cols-1 gap-10 max-xl:mx-auto place-content-center">
       {data.map((item, idx) => {
-        const isLast = idx === data.length - 1
+        // const isLast = idx === data.length - 1
 
         return (
           <AdvisoryBox
@@ -20,9 +21,9 @@ export default function Advisory() {
             Name={item.Name}
             Title={item.Title}
             Affiliation={item.Affiliation}
-            className={
-              isLast  ? 'xl:col-start-1 xl:col-end-3 xl:justify-self-center ' : ''
-            }
+            // className={
+            //   isLast  ? 'xl:col-start-1 xl:col-end-3 xl:justify-self-center ' : ''
+            // }
           />
         )
       })}
